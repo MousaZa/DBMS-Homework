18dab782cfec7b9299ebb65dc6b336f8ab14dacd
MousaZa <mous.zeydan@gmail.com>
2024-12-17 12:20:31 +0300

    firstCommit

---
+++ go_backend/go.mod
@@ -0,0 +1,29 @@
+module github.com/MousaZa/DBMS-Homework/go_backend
+
+go 1.22.2
+
+require (
+	github.com/hashicorp/go-hclog v1.6.3
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gorm.io/gorm v1.25.10 // indirect
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	gorm.io/driver/postgres v1.5.11
+)
---
+++ go_backend/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"net"
+	"os"
+
+	"github.com/MousaZa/DBMS-Homework/go_backend/storage"
+	"github.com/MousaZa/library-app-go/borrows/clients"
+	"github.com/MousaZa/library-app-go/borrows/models"
+	"github.com/MousaZa/library-app-go/borrows/server"
+	library "github.com/MousaZa/library-app-go/notifications/protos/notifications"
+	"github.com/hashicorp/go-hclog"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	log := hclog.Default()
+
+	log.Info("Starting borrows server" + os.Getenv("DB_HOST"))
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Error("Unable to get env", "error", err)
+	}
+
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		DBName:   os.Getenv("DB_NAME"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
+	db, err := storage.NewConnection(config)
+
+	if err != nil {
+		log.Error("Unable to connect to database", "error", err)
+		os.Exit(1)
+	}
+
+	err = models.MigrateBorrows(db)
+
+	if err != nil {
+		log.Error("Unable to migrate borrows", "error", err)
+		os.Exit(1)
+	}
+
+	// Notifications client
+	notificationsconn, err := grpc.NewClient("proxy:80", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	defer notificationsconn.Close()
+
+	notificationsClient := library.NewNotificationsClient(notificationsconn)
+
+	nc := clients.NewNotificationsClient(notificationsClient)
+
+	gs := grpc.NewServer()
+	cs := server.NewBorrowsServer(log, db, nc)
+
+	protos.RegisterBorrowsServer(gs, cs)
+
+	reflection.Register(gs)
+
+	l, err := net.Listen("tcp", ":9092")
+	if err != nil {
+		log.Error("unable to listen", err)
+		os.Exit(1)
+	}
+
+	gs.Serve(l)
+}
